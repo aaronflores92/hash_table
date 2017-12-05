6f9c917b773c85a57a2c9a77ab66b24f6ea112ad
aaronflores92 <31676547+aaronflores92@users.noreply.github.com>
2017-12-04 21:15:05 -0800

    fully functional hash table
    this file is a fully functional hash table implemented using an array[27] that lets you search for and add values. it will handle hash collisions using linear probing until the table is full. if the table is full, you will receive an error.

--- hash_table.cpp
+++ hash_table.cpp
@@ -10,6 +10,7 @@
 *****/
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -19,58 +20,83 @@ class HashTable
 		HashTable();
 		void add_val(int & v);
 		int search(int & v);
-		bool empty() {return size == 27
+		bool is_full() { return size == 27; }
 	
-	private:
-		int Hash(int & v);
-		int hash_array[27] = {};
+	//private:
+		int hash(int v);
+		int hash_table[27] = {};
 		int size = 0;
 };
 
 HashTable::HashTable()
 {
-	for(int i = 0; i < 27; i++) hash_array[i] = -1;
+	for(int i = 0; i < 27; i++) hash_table[i] = -1;
+}
+
+int HashTable::hash(int v)
+{
+	int hash_index = v % 27;
+	return hash_index;
 }
 
 void HashTable::add_val(int & v)
-//-This function takes the integer, determines the hash of that number by calling the above hash function, then adds it to the table using linear probing for collision resolution.
+//Takes the integer, determines the hash of that number by calling the above hash function, then adds it to the table using linear probing for collision resolution.
 {
-	std::cout << __func__ << " test value: " << v << endl;
-	std::cout << "Hashed value: " << Hash(v) << endl;
-	int hash_val = Hash(v), loop = 0;
-	while(hash_array[hash_val] != -1 && )
+	int loop = 0;
+	int hash_val = v;
+
+	while(hash_table[hash(hash_val)] != -1 && !is_full() && loop != 2)
 	{
-		if(hash_val == 27)
-		{
-			hash_val = 0;
-			loop = 1;
-		}
-		else hash_val++;
+		hash_val += 1;
+		if(hash(hash_val) == 0) loop++;
 	}
-	hash_array[hash_val] = v;
-	size++;
+	
+	if(!is_full())
+	{
+		hash_table[hash(hash_val)] = v;
+		size++;
+	}
+	else std::cout << "the hash table is full." << endl;
 }
 
 int HashTable::search( int & v)
-//Define a function that looks up a value, it takes an integer, return -1 if the value is not in the table.
+// Looks up a value, it takes an integer, return -1 if the value is not in the table.
 {
-	std::cout << __func__ << " test search val: " << v << endl;
-	return hash_array[Hash(v)];
+	for(int i = 0; i < 27; i++) if(hash_table[i] == v) return i;
+	return -1;
 }
 
-int HashTable::Hash(int & v)
-{
-	if(hash_table(Hash(v)] != -1) Hash(v + 1);
-	int index = v % 27;
-	return index;
-}
 
 int main()
 {
-	std::cout << "...Hash Table Main..." << endl;
+	std::cout << "...Hash Table Program..." << endl;
 	HashTable test;
-	int t = 29;
-	test.add_val(t);
-	std::cout << test.search(t) << "is the index" << endl;
+	std::cout << "Enter 'A' to add a value, 'S' to search for a value, or 'Q' to quit:" << endl;
+	std::string reply = "";
+	int i_val = 0;
+	int s_val = 0;
+	
+	while(getline(std::cin, reply) && reply != "Q")
+	{
+		if(reply == "A")
+		{
+			std::cout << "Enter a value to add to the hash table: " << endl;
+			std::cin >> i_val;
+			std::cout << "Adding value " << i_val << "..." << endl;
+			test.add_val(i_val);
+			std::cout << "Enter 'A' to add a value, 'S' to search for a value, or 'Q' to quit:" << endl;
+			std::cin.ignore();
+		}
+		else if(reply == "S")
+		{
+			std::cout << "Enter a value to search for in the hash table: " << endl;
+			std::cin >> s_val;
+			std::cout << "Searching for value " << s_val << "... An index of '-1' indicates the value does not exist in the hash table..." << endl;
+			std::cout << "The value " << s_val << " is in index " << test.search(s_val) << " of the hash table." << endl;
+			std::cout << "Enter 'A' to add a value, 'S' to search for a value, or 'Q' to quit:" << endl;
+			std::cin.ignore();
+		}
+		else std::cout << "That value was not valid, please enter a valid value ('A' to add a value, 'S' to search for a value, or 'Q' to quit):" << endl;
+	}
 	return 0;
 }
